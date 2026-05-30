abebe573a6305bb1925c61ecc63e7fbd836660fa
BrigidAn <ochimerema@gmail.com>
2026-05-31 01:11:50 +0200

    classes

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+// Car class with cloneCar using Symbol
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
