778ad9df534d1d68d1d4358b5908eeed79467aac
BrigidAn <ochimerema@gmail.com>
2026-05-31 00:26:26 +0200

    ES6

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+// For...of loops
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+// Object property value shorthand
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
